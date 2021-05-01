6d5c22bb63069b8d11f1460abe6f9c70ce089662
Amélia Alice <alice.cardoso10@gmail.com>
2021-05-01 20:30:14 -0300

    :tada: Starting project

---
+++ system-monitoring/src/main/java/com/amycardoso/systemmonitoring/SystemMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.amycardoso.systemmonitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemMonitoringApplication.class, args);
+	}
+
+}
---
+++ system-monitoring/src/test/java/com/amycardoso/systemmonitoring/SystemMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amycardoso.systemmonitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
