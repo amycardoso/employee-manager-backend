afc98da3ea286c2659f2f67ee3136b0f1ceecbd5
Amélia Alice <alice.cardoso10@gmail.com>
2021-05-08 13:47:50 -0300

    :sparkles: adding cors configurer

--- src/main/java/com/amycardoso/employeemanager/EmployeeManagerApplication.java
+++ src/main/java/com/amycardoso/employeemanager/EmployeeManagerApplication.java
@@ -2,12 +2,27 @@ package com.amycardoso.employeemanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class EmployeeManagerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeManagerApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:4200", "https://employee-manager-front.herokuapp.com")
+						.allowedMethods("GET", "POST", "PUT", "DELETE")
+						.maxAge(3600L);
+			}
+		};
 	}
 
 }
