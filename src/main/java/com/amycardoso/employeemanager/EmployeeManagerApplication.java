edbfea76b21894bf7ee9abca7642132a746e979c
Amélia Alice <alice.cardoso10@gmail.com>
2021-05-15 15:26:02 -0300

    :sparkles: adding in memory httpTrace and actuator cors config

--- src/main/java/com/amycardoso/employeemanager/EmployeeManagerApplication.java
+++ src/main/java/com/amycardoso/employeemanager/EmployeeManagerApplication.java
@@ -1,6 +1,9 @@
 package com.amycardoso.employeemanager;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,6 +11,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class EmployeeManagerApplication {
+
+	@Value("${allowed-origins}")
+	protected String[] allowedOrigins;
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeManagerApplication.class, args);
@@ -18,11 +24,16 @@ public class EmployeeManagerApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:4200", "https://employee-manager-front.herokuapp.com")
+				registry.addMapping("/**").allowedOrigins(allowedOrigins)
 						.allowedMethods("GET", "POST", "PUT", "DELETE")
 						.maxAge(3600L);
 			}
 		};
+	}
+
+	@Bean
+	public HttpTraceRepository httpTraceRepository() {
+		return new InMemoryHttpTraceRepository();
 	}
 
 }
